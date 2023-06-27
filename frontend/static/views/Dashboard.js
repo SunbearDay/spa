8821b777b3ac6d015db267f3bec26e2e17e58868
sunbear <sunbearday@outlook.com>
2023-06-26 17:56:18 -0700

    save

---
+++ frontend/static/views/Dashboard.js
@@ -0,0 +1,20 @@
+import AbstractView from "./AbstractView.js"
+
+export default class extends AbstractView {
+    constructor() {
+        super()
+        this.setTitle('Dashboard')
+    }
+
+    async getHtml() {
+        return `
+            <h1>Welcome back, DOM</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vero quasi, aut debitis hic, sunt dolor dolores vitae dicta quisquam deserunt illum tempora molestias a? Deserunt pariatur quod explicabo iste.
+            </p>
+            <p>
+                <a href='/posts' data-link>View recent posts</a>
+            </p>
+        `
+    }
+}
